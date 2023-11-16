b7d6411993f8b4a0d11d62a853d8d3194a6ea33b
Martin Dutra <martindsq@hotmail.com>
2023-11-16 15:24:36 -0300

    add utf-8 and es-uy to settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -110,7 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+DEFAULT_CHARSET = 'utf-8'
+
+LANGUAGE_CODE = 'es-uy'
 
 TIME_ZONE = 'UTC'
 
